74ecb707a624915a625389972c5d7c0548059435
j-forgac <62855827+j-forgac@users.noreply.github.com>
2021-12-16 00:19:50 +0100

    wtf2

--- src/main/kotlin/y2021/Day15v2.kt
+++ src/main/kotlin/y2021/Day15v2.kt
@@ -11,6 +11,7 @@ private val totalSize = input.first().size.toDouble().pow(2).toInt() - 1
 private val graph: MutableMap<Int, Node> = mutableMapOf()
 private val dist: MutableMap<Int,Int> = (0..totalSize).associateWith { Int.MAX_VALUE }.toMutableMap()
 private val unVisited: MutableSet<Int> = (0..totalSize).toMutableSet()
+private val visit: MutableSet<Int> = mutableSetOf()
 
 fun main() {
 	val time = measureTimeMillis {
@@ -27,12 +28,16 @@ class Day15v2() {
 		var curNode = 0
 		while (unVisited.contains(99)) {
 			unVisited.remove(curNode)
+			visit.add(curNode)
 			graph[curNode]!!.adjacent.forEach {
 				val distToNewNode = dist[curNode]!!.plus(graph[it]!!.weight)
 				if(distToNewNode < dist[it]!!) dist[it] = distToNewNode
 			}
 			if(unVisited.contains(99)){
 				println(curNode)
+				println(unVisited)
+				println(visit)
+				println(graph[curNode]!!.adjacent.filter { it in unVisited })
 				println("ok: ${graph[curNode]!!.adjacent.filter { it in unVisited }.sortedBy { dist[it]}[0]}")
 				println("${graph[curNode]!!.adjacent} : ${graph[curNode]!!.weight}")
 				println("picks: ${graph[curNode]!!.adjacent.filter { it in unVisited }.minOf {it}}")
