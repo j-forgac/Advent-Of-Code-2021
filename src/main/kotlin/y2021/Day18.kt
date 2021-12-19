2e96dc48942d3997036516f11b786cdd4079be59
j-forgac <62855827+j-forgac@users.noreply.github.com>
2021-12-19 10:55:45 +0100

    finally...

--- src/main/kotlin/y2021/Day18.kt
+++ src/main/kotlin/y2021/Day18.kt
@@ -3,40 +3,60 @@ package y2021
 import java.io.File
 import kotlin.math.ceil
 import kotlin.math.floor
+import kotlin.system.measureTimeMillis
 
 private val input = File("./src/main/kotlin/inputs/input18.txt").readLines()
 
 
 fun main() {
 	val day = Day18()
-	day.getFinal("[[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]],[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]]")
-	//day.getFinal("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]")
-}
-
-class Day18() {
-	fun part1() {
-		var totalSum = ""
-		input.forEach {
-
-		}
+	val timeInMillis = measureTimeMillis {
+		day.part1()
 	}
 
-	fun getFinal(input: String) {
-		println(input)
+	day.part2()
+	println("the time was: $timeInMillis ms")
+}
+
+class Day18 {
+	fun part1() {
+		var totalSum = getFinal("[${input[0]},${input[1]}]")
+		input.drop(2).forEach {
+//			println(totalSum)
+//			println("+ $it")
+			totalSum = getFinal("[$totalSum,$it]")
+//			println("= $totalSum\n")
+		}
+		println(magnitude(totalSum))
+	}
+
+	fun part2() {
+		var highestMagnitude = 0
+		input.forEach { first ->
+			input.forEach { second ->
+				if (first != second) {
+					val temp = magnitude(getFinal("[$first,$second]"))
+//					println("[$first,$second] \n = $temp")
+					if (temp > highestMagnitude) highestMagnitude = temp
+				}
+			}
+		}
+		println(highestMagnitude)
+	}
+
+	fun getFinal(input: String): String {
 		var prev = ""
 		var cur = input
+		var i = 0
 		do {
 			prev = cur
 			cur = explode(prev)
-			if(cur == prev) cur = splitBig(cur)
-			println(cur)
-			/*do {
-				prev = cur
-				cur = splitBig(prev)
-				cur = explode(cur)
-			} while (cur != prev)*/
+			if (cur == prev) {
+				cur = splitBig(cur)
+			}
+			i++
 		} while (cur != prev)
-		println(cur)
+		return cur
 	}
 
 	fun explode(input: String): String {
@@ -47,22 +67,15 @@ class Day18() {
 				']' -> depth--
 			}
 			if (depth == 5) {
-				println()
-				println(input)
 				val pair = Regex(""".{${i}}(\d+,\d+).*""").find(input)!!.destructured.component1().split(",")
 					.map { it.toInt() }
-				println(pair)
-				val oldStr = input.split(pair.toString().replace(" ", ""), limit = 2)
+				val oldStr = listOf(input.substring(0, i), input.substring(i + pair.toString().length - 1))
 				var replacePair = ",0"
 				var (suffixL, numberL, prefixL) = Regex("""(.*?)(\d+)(.*)""").find(oldStr[0].reversed())?.destructured?.toList()
 					?.map { it.reversed() }
-					?: listOf(oldStr[0], "", "")
+					?: listOf("", "", oldStr[0])
 				var (prefixR, numberR, suffixR) = Regex("""(.*?)(\d+)(.*)""").find(oldStr[1])?.destructured?.toList()
 					?: listOf(oldStr[1], "", "")
-				println("divided: ")
-				println("$prefixL <- : -> $numberL <- : -> $suffixL")
-				println("$prefixR : $numberR : $suffixR")
-				println("divided:")
 				if (!"$prefixL$numberL$suffixL".last().isDigit()) replacePair = replacePair.substring(1)
 
 				//increase value of the closest numbers (if there is none, the replacement of pair 0 is the first number -> no comma needed)
@@ -70,7 +83,6 @@ class Day18() {
 
 				if (numberR.isNotEmpty()) numberR = (numberR.toInt() + pair[1]).toString()
 
-				println("res: $prefixL$numberL$suffixL$replacePair$prefixR$numberR$suffixR \n")
 				return "$prefixL$numberL$suffixL$replacePair$prefixR$numberR$suffixR"
 			}
 		}
@@ -79,16 +91,18 @@ class Day18() {
 
 	fun splitBig(input: String): String {
 		if (!Regex(".*?(\\d{2}).*").matches(input)) return input
-		println("split: $input")
 		val (num) = Regex(".*?(\\d{2}).*").find(input)!!.destructured
-		println(
-			"splitted: $num --> ${
-				input.replaceFirst(
-					num,
-					"[${floor(num.toFloat() / 2).toInt()},${ceil(num.toFloat() / 2).toInt()}]"
-				)
-			}"
-		)
 		return input.replaceFirst(num, "[${floor(num.toFloat() / 2).toInt()},${ceil(num.toFloat() / 2).toInt()}]")
+	}
+
+	fun magnitude(input: String): Int {
+		var res = input
+		while (Regex(""".*?\d+,\d+.*""").matches(res)) {
+			val pair = Regex("""(\d+,\d+)""").find(res)!!.destructured.component1().split(",")
+				.map { it.toInt() }
+			val result = pair[0] * 3 + pair[1] * 2
+			res = res.replaceFirst(pair.toString().replace(" ", ""), result.toString())
+		}
+		return res.removePrefix("[").removeSuffix("]").toInt()
 	}
 }
